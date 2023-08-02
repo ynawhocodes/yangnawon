434aace7c3afa5c6760f00b6bca659945d800583
yanwhocodes <ynawhocodes@gmail.com>
2023-08-02 23:44:56 +0900

    [feat] February section rotation 외 작업

---
+++ apps/web/src/app/(pages)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Cube from "../(components)/Cube";
+import FebruarySection from "../(components)/FebruarySection";
+
+const Home = () => {
+  return (
+    <>
+      <FebruarySection />
+    </>
+  );
+};
+export default Home;
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,16 @@
 import "../styles/globals.css";
 // include styles from the ui package
 import "ui/styles.css";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-zinc-900">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en" className="bg-white">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
